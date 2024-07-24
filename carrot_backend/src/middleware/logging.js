a34f564f4d23a61fc5009f2a9dec368f3c601d2f
SeongEun <bongfam2@naver.com>
2024-07-24 16:10:44 +0900

    rollback

--- carrot_backend/src/middleware/logging.js
+++
@@ -1,5 +0,0 @@
-module.exports = (req, res, next) => {
-  let clientIp = req.ip;
-  console.log(`${clientIp.replace("::ffff:", "")} : ${req.path}`);
-  next();
-};
--- carrot_flutter/lib/src/shared/global.dart
+++
@@ -1,6 +0,0 @@
-//전역변수 -> 앱을 종료하였다가 다시 실행해도 남아있는 변수
-class Global {
-  // static const apiRoot = 'https://sns.jinhyung.kim';
-  // static const apiRoot = "http://192.168.56.1:3000";
-  static const apiRoot = "http://localhost:3000";
-}
